2a480a52372947bfe0d20fdfef68c72a03cc3e09
Marco Maddo (CEO TSSTI Tecnologia) <profmaddo@gmail.com>
2024-06-04 09:54:10 -0400

    Cluster Swarm Local com o Vagrant Ubuntu 2 nodes

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-4uegmw2k3sfal5fch8zple8i5fpfk5hs1jjygjykbn8yc7khrh-dpjpxt5cq1grqp3rms8s6qa04 192.168.56.100:2377
